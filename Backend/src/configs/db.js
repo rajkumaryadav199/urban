5ceaecbc8bd403bc488b49fe176cebdbc4164615
rajkumaryadav199 <kuraj10101999@gmail.com>
2023-01-14 20:05:07 +0530

    initial commit

--- Backend/src/configs/db.js
+++ Backend/src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect("mongodb+srv://saniya:saniya_123@cluster0.9gcpvp1.mongodb.net/test?retryWrites=true&w=majority");
+  return  mongoose.connect("mongodb+srv://urban:urban-123@urban.mzd332o.mongodb.net/urban?retryWrites=true&w=majority");
   // return mongoose.connect("mongodb+srv://saikiran11471:saikiran11471@cluster0.b1mxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 };
 ///mongodb+srv://Saniya:saniya@cluster0.7im7o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
