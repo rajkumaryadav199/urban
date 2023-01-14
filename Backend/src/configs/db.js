e27af2eaffe899e681b4d5b8a54316f58ab0a846
rajkumaryadav199 <kuraj10101999@gmail.com>
2023-01-14 20:08:33 +0530

    done

--- Backend/src/configs/db.js
+++ Backend/src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return  mongoose.connect("mongodb+srv://urban:urban-123@urban.mzd332o.mongodb.net/urban?retryWrites=true&w=majority");
-  // return mongoose.connect("mongodb+srv://saikiran11471:saikiran11471@cluster0.b1mxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+ 
 };
 ///mongodb+srv://Saniya:saniya@cluster0.7im7o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 //mongodb+srv://saniya:saniya_143@cluster0.2rll2j3.mongodb.net/?retryWrites=true&w=majority
